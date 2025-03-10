dff12f67a821e6f143e315e4ca02b2e7221e7f1d
Ophelia Beatrice de Sica <GoGoOtaku@gmail.com>
2025-03-10 17:40:25 +0100

    Port various soft render primitives from ASM to C
    This commit ports the MASM code for:
    * TrapezoidRenderPITAN
    * TrapezoidRenderPITA
    * TrapezoidRenderPITIA
    * TrapezoidRenderPITA15
    * TrapezoidRenderPITIA_RGB_555
    * TrapezoidRenderPITA24
    * TrapezoidRenderPITIA_RGB_888
    * TrapezoidRenderPIZ2TAN
    * TrapezoidRenderPIZ2TA
    * TrapezoidRenderPIZ2TIA
    * TrapezoidRenderPIZ2TA15
    * TrapezoidRenderPIZ2TIA_RGB_555
    * TrapezoidRenderPIZ2TA24
    * TrapezoidRenderPIZ2TIA_RGB_888
    * ScanLinePITPD64
    * ScanLinePITPD128
    * ScanLinePITPD256
    * ScanLinePITPD1024
    * ScanLinePITIP256_RGB_555
    * ScanLinePITIPB256_RGB_555
    * ScanLinePITIPB256_RGB_565

---
+++ drivers/pentprim/t_piza.c
@@ -0,0 +1,406 @@
+#include "brender.h"
+
+#include "drv.h"
+#include "priminfo.h"
+#include "work.h"
+#include <complex.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+// I have no idea why this is a thing
+// but the optimizer should throw this out anyway
+static const bool WRAP = 1;
+static const bool WRAPROW = 1;
+static const bool TRANSP = 1;
+
+static inline void scan_inc(const bool carry, const bool light, const short sbpp) {
+    // -- Update current  -- //
+    // TODO: This seems incredibly bug prone and possibly wrong???
+    br_fixed_ls current = *(br_fixed_ls*)work.awsl.start;
+    br_fixed_ls d = work.main.d_i;
+
+    if (light) {
+        current = work.pi.current;
+        d = carry ? work.pi.d_carry : work.pi.d_nocarry;
+    }
+    work.pz.current += carry ? work.pz.d_carry : work.pz.d_nocarry;
+    work.pi.current = current + d;
+
+    // -- Update u_current -- //
+    unsigned u_current = work.awsl.u_current;
+    unsigned du = carry ? work.awsl.du_carry : work.awsl.du_nocarry;
+
+    unsigned new_u_current = u_current + du;
+    work.awsl.u_current = new_u_current;
+
+    int CF = 0;
+    // Unsigned overflow
+    if (new_u_current < u_current) {
+        CF = -1;
+    }
+
+    if (WRAP) {
+        // -- Update u_int_current -- //
+        short u_int_current = work.awsl.u_int_current; // dword ptr
+        u_int_current += carry ? work.awsl.du_int_carry : work.awsl.du_nocarry;
+        u_int_current -= CF;
+        work.awsl.u_int_current = u_int_current;
+    }
+
+    // -- Update v_current -- //
+    work.awsl.v_current += carry ? work.awsl.dv_carry : work.awsl.dv_nocarry;
+
+    // -- Update source_current -- //
+    char* source_current = work.awsl.source_current;
+    source_current += carry ? work.awsl.dsource_carry : work.awsl.dsource_nocarry;
+
+    CF *= sbpp;
+    source_current -= CF;
+
+    if (CF) {
+        source_current += work.texture.stride_b;
+    }
+
+    work.awsl.source_current = source_current;
+}
+
+static inline char* scan_forward(const bool light, const bool bump, const uint8_t sbpp, const uint8_t dbpp) {
+    int noffset = work.bump.base - work.texture.base;
+
+    char* start = work.awsl.start;
+    const char* end = work.awsl.end; // _end?
+
+    unsigned u_current = work.awsl.u_current;
+    unsigned v_current = work.awsl.v_current;
+
+    short _t = work.awsl.u_int_current;
+    short swapped_u_int_current = (_t >> 8) | (_t << 8);
+    u_current = swapped_u_int_current;
+
+    char* source_current = work.awsl.source_current;
+    char* zstart = work.awsl.zstart;
+
+    while (start < end) {
+        bool transparent = false;
+        if (TRANSP) {
+            for (uint8_t i = 0; i < sbpp; ++i) {
+                transparent = transparent && (source_current[i] == 0);
+            }
+        }
+
+        // ---- 16 bit z test ---- //
+
+        // Z values are big endian for some reason
+        br_uint_8 z_low = ((br_uint_8*)&work.pz.currentpix)[2];
+        br_uint_8 z_high = ((br_uint_8*)&work.pz.currentpix)[3];
+
+        br_uint_16 z_currentpix = (z_high << 8) | z_low;
+        br_uint_16 z_test_start = (zstart[1] << 8) | zstart[0];
+
+        if (z_test_start < z_currentpix) {
+            // z test failed
+            // Set transparent to skip
+            transparent = true;
+        } else {
+            // Store z as big endian
+            zstart[0] = z_low;  // Store low byte
+            zstart[1] = z_high; // Store high byte
+        }
+
+        if (light) {
+            if (!transparent) {
+                br_uint_8 lighting_index = ((br_uint_8*)&work.pi.currentpix)[2];
+                br_uint_8 texel = *source_current;
+
+                uint32_t offset = 0;
+                offset = (lighting_index << 8) | texel;
+                br_uint_8* shade_table_ptr = work.shade_table;
+
+                shade_table_ptr += offset * dbpp; // Light texel
+                *start = *shade_table_ptr;        // Store texel
+            }
+            work.pi.currentpix += work.pi.grad_x;
+
+        } else if (bump) {
+            if (!transparent) {
+                br_uint_8 texel = *source_current;
+                br_uint_8 normal = *(source_current + noffset);
+                br_uint_8 light_level = *(normal + work.lighting_table);
+
+                uint32_t offset = (light_level << 8) | texel;
+
+                const br_uint_8* light_texel = work.shade_table + offset;
+                *start = *light_texel;
+            }
+
+        } else {
+            if (!transparent) {
+                for (br_uint_8 i = 0; i < sbpp; ++i) {
+                    start[i] = source_current[i];
+                }
+            }
+        }
+
+        start += dbpp;
+        zstart += 2;
+        work.pz.currentpix += work.pz.grad_x;
+
+        // NOTE FROM MASM: Redo this with high and low halfwords of du swapped
+
+        unsigned new_u_current = u_current + work.awsl.du;
+
+        int CF = 0;
+        if (new_u_current < u_current) {
+            // Carry (unsigned Overflow)
+            CF = -1;
+        }
+        u_current = new_u_current;
+        source_current += work.awsl.dsource;
+
+        u_current -= CF;
+
+        CF *= sbpp;
+
+        source_current -= CF;
+
+        u_current += work.awsl.du_int;
+
+        unsigned new_v_current = v_current + work.awsl.dv;
+        if (new_v_current < v_current) {
+            source_current += work.texture.stride_b;
+        }
+        v_current = new_v_current;
+
+        // test ah,ah
+        if ((u_current & 0xFF00)) {
+            br_uint_32 width_p = work.texture.width_p;
+            u_current += width_p;
+            source_current += width_p * sbpp;
+        }
+
+        if ((void*)source_current < work.texture.base) {
+            // Have we underrun?
+            source_current += work.texture.size;
+        }
+    }
+
+    return source_current;
+}
+
+static inline char* scan_backward(const bool light, const bool bump, const uint8_t sbpp, const uint8_t dbpp) {
+    int noffset = work.bump.base - work.texture.base;
+
+    char* start = work.awsl.start;
+    const char* end = work.awsl.end; // _end?
+
+    unsigned u_current = work.awsl.u_current;
+    unsigned v_current = work.awsl.v_current;
+
+    short _t = work.awsl.u_int_current;
+    short swapped_u_int_current = (_t >> 8) | (_t << 8);
+    u_current = swapped_u_int_current;
+
+    char* source_current = work.awsl.source_current;
+    char* zstart = work.awsl.zstart;
+
+    while (start > end) {
+        work.pz.currentpix -= work.pz.grad_x;
+
+        if (light) {
+            work.pi.currentpix -= work.pi.grad_x;
+            /// ???
+        }
+
+        // TODO: In MASM: Redo this with high and low halfwords of du swapped
+        unsigned new_u_current = u_current + work.awsl.du;
+        int CF = 0;
+        if (new_u_current < u_current) {
+            CF = -1;
+        }
+
+        source_current += work.awsl.dsource;
+        u_current -= CF;
+        u_current += work.awsl.du_int;
+
+        source_current -= CF * sbpp;
+
+        unsigned new_v_current = v_current + work.awsl.dv;
+        if (new_v_current < v_current) {
+            source_current += work.texture.stride_b;
+        }
+        v_current = new_v_current;
+
+        // test ah,ah
+        if ((u_current & 0xFF00)) {
+            br_uint_32 width_p = work.texture.width_p;
+            u_current += width_p;
+            source_current += width_p * sbpp;
+        }
+
+        if ((void*)source_current < work.texture.base)
+            source_current += work.texture.size; // _size
+
+        zstart -= 2;
+        start -= dbpp;
+
+        if (TRANSP) {
+            bool transparent = false;
+            for (uint8_t i = 0; i < sbpp; ++i) {
+                transparent = transparent && (source_current[i] == 0);
+            }
+            if (transparent)
+                continue; // Skip if transparent
+        }
+
+        // ---- 16 bit z test ---- //
+
+        // Z values are big endian for some reason
+        br_uint_8 z_low = ((br_uint_8*)&work.pz.currentpix)[2];
+        br_uint_8 z_high = ((br_uint_8*)&work.pz.currentpix)[3];
+
+        br_uint_16 z_currentpix = (z_high << 8) | z_low;
+        br_uint_16 z_test_start = (zstart[1] << 8) | zstart[0];
+
+        if (z_test_start < z_currentpix) {
+            // z test failed
+            continue;
+        }
+        // Store z as big endian
+        zstart[0] = z_low;  // Store low byte
+        zstart[1] = z_high; // Store high byte
+
+        switch (sbpp) {
+        case 1:;
+            ;
+            br_uint_8 texel = *source_current; // Get texel
+            br_uint_8 light_index = ((br_uint_8*)&work.pi.currentpix)[2];
+            switch (dbpp) {
+            case 1:
+                if (light) {
+                    texel = *(br_uint_8*)((light_index << 8) + texel + work.shade_table); // Light texel
+                }
+
+                if (bump) {
+                    br_uint_8 light_level = *(work.lighting_table + *(br_uint_8*)(source_current + noffset)); // calculate light level from normal
+                    texel = *(br_uint_8*)((light_level << 8) + texel + work.shade_table);                     // Light texel
+                }
+
+                *start = texel;
+                break;
+            case 2:
+                if (light)
+                    *(br_uint_16*)start = *(br_uint_16*)(work.shade_table + ((light_index << 8) + texel) * 2);
+                break;
+            case 3:
+                if (light) {
+                    *(br_uint_16*)start = *(br_uint_16*)(work.shade_table + ((light_index << 8) + texel) * 4);
+                    *(br_uint_8*)(start + 2) = ((br_uint_8*)(work.shade_table + ((light_index << 8) + texel) * 4))[2];
+                }
+                break;
+            }
+            break;
+        case 2:
+            *(br_uint_16*)start = *(br_uint_16*)source_current;
+            break;
+        case 3:
+            *(br_uint_16*)start = *(br_uint_16*)source_current;
+            ((br_uint_8*)start)[3] = ((br_uint_8*)source_current)[3];
+            break;
+        case 4:
+            *(br_uint_32*)start = *(br_uint_32*)source_current;
+            break;
+        }
+    }
+
+    return source_current;
+}
+
+static inline void triangle_render_z2(const bool light, const bool bump, const uint8_t sbpp, const uint8_t dbpp) {
+    struct scan_edge* edge = work.awsl.edge;
+
+    for (; edge->count; --edge->count) {
+        char* start = work.awsl.start;
+        char* end = work.awsl.end; // _end?
+
+        work.pz.currentpix = work.pz.current;
+
+        if (light) {
+            work.pi.currentpix = work.pi.current;
+        }
+
+        // Unused variable: // char* source_current;
+        char* zstart = work.awsl.zstart;
+
+        if (start < end)
+            /* source_current = */ scan_forward(light, bump, sbpp, dbpp);
+        else
+            /* source_current = */ scan_backward(light, bump, sbpp, dbpp);
+
+        // Per scan line updates
+        br_int_32 new_f = edge->f + edge->d_f;
+        br_uint_32 d_i = edge->d_i;
+        end = work.awsl.end;
+
+        int CF = 0;
+        if (new_f < edge->f) {
+            CF = 1;
+        }
+        edge->f = new_f;
+
+        end += (d_i + CF) * dbpp; // work.awsl.end += dbpp*(edge->d_i+carry)
+
+        work.awsl.end = end;
+
+        new_f = work.main.f + work.main.d_f;
+        CF = 0;
+        if (new_f < work.main.f)
+            CF = 1;
+        work.main.f = new_f;
+
+        start = work.awsl.start;
+        d_i = work.main.d_i;
+
+        if (CF) {
+            d_i += CF;
+        }
+
+        // This is not used for some reason
+        zstart = zstart + 2 * d_i;
+        start += d_i * dbpp;
+        work.awsl.start = start;
+        work.awsl.zstart = zstart;
+        scan_inc(CF, light, sbpp);
+
+        br_uint_16 u_int_current = work.awsl.u_int_current;
+        // test bh, bh
+        // Test sign of low(?) halfword
+        if ((u_int_current & 0xFF00)) {
+            br_uint_32 width_p = work.texture.width_p;
+            work.awsl.u_int_current += width_p;
+            work.awsl.source_current += width_p * sbpp;
+        }
+
+        if ((void*)work.awsl.source_current < work.texture.base)
+            work.awsl.source_current += work.texture.size;
+    }
+}
+
+#if PARTS & PART_BUMP
+void BR_ASM_CALL TrapezoidRenderPIZ2TAN() { triangle_render_z2(false, true, 1, 1); } // Normal map
+#endif
+
+#if PARTS & PART_8Z
+void BR_ASM_CALL TrapezoidRenderPIZ2TA() { triangle_render_z2(false, false, 1, 1); }
+void BR_ASM_CALL TrapezoidRenderPIZ2TIA() { triangle_render_z2(true, false, 1, 1); }
+#endif
+
+#if PARTS & PART_15Z
+void BR_ASM_CALL TrapezoidRenderPIZ2TA15() { triangle_render_z2(false, false, 2, 2); }
+void BR_ASM_CALL TrapezoidRenderPIZ2TIA_RGB_555() { triangle_render_z2(true, false, 1, 2); }
+#endif
+
+#if PARTS & PART_24Z
+void BR_ASM_CALL TrapezoidRenderPIZ2TA24() { triangle_render_z2(false, false, 3, 3); }
+void BR_ASM_CALL TrapezoidRenderPIZ2TIA_RGB_888() { triangle_render_z2(true, false, 1, 3); }
+#endif
+
