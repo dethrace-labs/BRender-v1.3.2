a6db575963005fe86bfaceeffcb25f449704eddf
Anonymous Maarten <madebr@users.noreply.github.com>
2026-03-31 21:03:37 +0200

    glrend: print name of GL error (#26)

--- drivers/glrend/glassert.c
+++ drivers/glrend/glassert.c
@@ -3,8 +3,18 @@
 // todo assert or warn based on a flag
 void GL_AssertOrWarnIfError(char *file, int line) {
     int e;
+    const char *kind;
     e = glGetError();
-    if (e != 0) {
-        BrWarning("GL error: %s:%d %d\n", file, line, e);
+    if (e == GL_NO_ERROR) {
+        return;
     }
+    switch (e) {
+        case GL_INVALID_ENUM:                   kind = "GL_INVALID_ENUM"; break;
+        case GL_INVALID_VALUE:                  kind = "GL_INVALID_VALUE"; break;
+        case GL_INVALID_OPERATION:              kind = "GL_INVALID_OPERATION"; break;
+        case GL_INVALID_FRAMEBUFFER_OPERATION:  kind = "GL_INVALID_FRAMEBUFFER_OPERATION"; break;
+        case GL_OUT_OF_MEMORY:                  kind = "GL_OUT_OF_MEMORY"; break;
+        default:                                kind = "<unknown>"; break;
+    }
+    BrWarning("GL error: %s:%d %s (0x%x)\n", file, line, kind, e);
 }
